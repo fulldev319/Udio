aaceab97b884104aaac172d704ac5ea6d0aaaaec
fulldev319 <maslea319@gmail.com>
2024-07-26 15:43:42 -0400

    resolve console warning related to key indexing in list

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -44,7 +44,7 @@ function SongList(props: { songs: Song[]; onSelect: (which: number) => void }) {
   return (
     <div className="song-list">
       {props.songs.map((song, i) => (
-        <SongListEntry song={song} onSelect={() => props.onSelect(i)} />
+        <SongListEntry key={`${i}-${song.song_path}`} song={song} onSelect={() => props.onSelect(i)} />
       ))}
     </div>
   );
