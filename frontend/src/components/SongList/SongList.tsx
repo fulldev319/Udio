b4638ac25e29fbfae433ab0c7d5c808ef4537f29
fulldev319 <maslea319@gmail.com>
2024-07-28 19:33:05 -0400

    Make responsive page

--- frontend/src/components/SongList/SongList.tsx
+++ frontend/src/components/SongList/SongList.tsx
@@ -7,7 +7,11 @@ function SongList(props: { songs: Song[]; onSelect: (which: number) => void }) {
   return (
     <div className="song-list">
       {props.songs.map((song, i) => (
-        <SongListEntry key={`${i}-${song.song_path}`} song={song} onSelect={() => props.onSelect(i)} />
+        <SongListEntry
+          key={`${i}-${song.song_path}`}
+          song={song}
+          onSelect={() => props.onSelect(i)}
+        />
       ))}
     </div>
   );
