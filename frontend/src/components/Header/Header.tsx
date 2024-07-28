cb881b26c216b460b213b8873b109c67170cef70
fulldev319 <maslea319@gmail.com>
2024-07-28 16:57:39 -0400

    fix header style

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -6,10 +6,14 @@ function Header() {
   return (
     <div className="header">
       <h1 className="logo">musicblob</h1>
-      <input type="text" placeholder="What do you want to hear?" />
-      <input type="button" value="Search" onClick={NotImplemented} />
-      <a onClick={NotImplemented}>Sign Up</a>
-      <a onClick={NotImplemented}>Login</a>
+      <div>
+        <input type="text" placeholder="What do you want to hear?" />
+        <input type="button" value="Search" onClick={NotImplemented} />
+      </div>
+      <div>
+        <a onClick={NotImplemented}>Sign Up</a>
+        <a onClick={NotImplemented}>Login</a>
+      </div>
     </div>
   );
 }
