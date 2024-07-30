d22dd4b2f15236bca487a6d5e8735676e62453ed
fulldev319 <maslea319@gmail.com>
2024-07-30 12:07:33 -0400

    Remove NotImplemented component

--- frontend/src/components/NotImplemented/NotImplemented.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function NotImplemented() {
-  window.alert("Not implemented!");
-}
-
-export default NotImplemented;
--- frontend/src/components/index.tsx
+++ frontend/src/components/index.tsx
@@ -3,4 +3,3 @@ export { default as PlayBar } from './PlayBar/PlayBar';
 export { default as SongList } from './SongList/SongList';
 export { default as SongListEntry } from './SongListEntry/SongListEntry';
 export { default as PlayList } from './PlayList/PlayList';
-export { default as NotImplemented } from './NotImplemented/NotImplemented';
